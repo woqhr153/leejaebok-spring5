f028b681fcc7bdecbffdf556c3f7f4ca0a04e2f1
boramcom@daum.net <boramcom@daum.net>
2021-05-25 21:11:40 +0900

    pom.xml 하단에 plugin 항목의 버전을 1.6에서 1.8로 변경

---
+++ src/test/java/kr/or/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package kr.or.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// classpath > exclude test code unchecked 해야지만, 오류안남.
+		System.out.println("헬로 자바");
+	}
+
+}
