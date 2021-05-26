aca07d6fd89e65f7c124e1a618a909d25728ef47
boramcom@daum.net <boramcom@daum.net>
2021-05-26 10:36:37 +0900

    어제 마무리 실습

--- src/test/java/kr/or/test/HelloWorld.java
+++ src/test/java/kr/or/test/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// classpath > exclude test code unchecked 해야지만, 오류안남.
-		System.out.println("헬로 자바");
+		System.out.println("헬로 자바 !!!!");
 	}
 
 }
