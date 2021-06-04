4706c9d9243f436ffbb1215b83c1ed539faca858
boramcom@daum.net <boramcom@daum.net>
2021-06-04 12:50:43 +0900

    4교시 커밋2

--- src/test/java/com/edu/test/DataSourceTest.java
+++ src/test/java/com/edu/test/DataSourceTest.java
@@ -47,9 +47,10 @@ public class DataSourceTest {
 		//stmt객체가 없으면, 개발자가 SQL인젝션 방지코딩을 넣어야 합니다.
 		//Insert쿼리문장만듬(아래)
 		//예전 방식으로 더미데이터(샘플데이터)를 100개를 입력합니다.
-		for(int cnt=0;cnt<100;cnt++) {
-			stmt.executeQuery("insert into dept02 values("+cnt+",'디자인부','경기도')");
-		}
+		/*
+		 * for(int cnt=0;cnt<100;cnt++) {
+		 * stmt.executeQuery("insert into dept02 values("+cnt+",'디자인부','경기도')"); }
+		 */
 		//인서트,업데이트,삭제시 sql디벨러퍼에서는 커밋이 필수지만, 외부java클래스에서는 자동커밋이 됩니다.
 		//테이블에 입력되어 있는 레코드셋를 select 쿼리 stmt문장으로 가져옴(아래)
 		ResultSet rs = stmt.executeQuery("select * from dept02 order by deptno");//20년전 작업방식
@@ -59,6 +60,8 @@ public class DataSourceTest {
 			logger.debug(rs.getString("deptno")+" "+rs.getString("dname")+
 					" "+rs.getString("loc"));
 		}
+		stmt = null;//메모리 반환
+		rs = null;//메모리 반환
 		connection = null;//메모리 초기화
 	}
 	@Test
