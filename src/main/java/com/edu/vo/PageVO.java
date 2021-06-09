6a9c9b069ec140e5319fc0b0bf9dd4616deef1ab
boramcom@daum.net <boramcom@daum.net>
2021-06-09 09:50:41 +0900

    1교시 커밋

--- src/main/java/com/edu/vo/PageVO.java
+++ src/main/java/com/edu/vo/PageVO.java
@@ -8,9 +8,9 @@ package com.edu.vo;
   * 검색에 사용되는 변수(쿼리변수만): 검색어(search_keyword), 검색조건(search_type)
  */
 public class PageVO {
-	private int queryStartNo;//쿼리전용 변수
-	private int queryPerPageNum;//쿼리전용
-	private Integer page;//jsp에서발생 자바전용.int인데 Null값을 허용.
+	private int queryStartNo;//쿼리전용 변수, 페이징쿼리에서 시작 페이지 인덱스번호표시 변수
+	private int queryPerPageNum;//쿼리전용, 페이징쿼리에서 1페이지당 출력할 개수표시 변수
+	private Integer page;//jsp에서발생 선택한 페이지 번호변수. 자바전용.int인데 Null값을 허용.
 	private int perPageNum;//UI 하단에 보여줄 페이징 개수 계산에 필요
 	private int totalCount;//계산식의 기초값으로 이 전체개수가 구해진 이후 페이징등의 계산식이 진행됩니다.
 	private int startPage;//위 perPageNum으로 구하는 UI하단페이지시작번호
@@ -23,7 +23,7 @@ public class PageVO {
 	public int getQueryStartNo() {
 		//this.page-1하는 이유는 jsp에서는 1,2,3...받지만,
 		//쿼리에서는 0,1,2...으로 사용되기 때문에. page*페이지당보여줄개수
-		queryStartNo = (this.page-1)*queryPerPageNum;
+		queryStartNo = (this.page-1)*queryPerPageNum;//쿼리에서 시작페이지의 인덱스 번호로 사용
 		return queryStartNo;
 	}
 	public void setQueryStartNo(int queryStartNo) {
@@ -63,7 +63,13 @@ public class PageVO {
 		//jsp에서 클릭한 페이지번호 예로 1 부터 10까지는 클릭하면, 임시 끝페이지가 10.
 		//만약에 11페이지를 클릭하면, 임시 끝페이지가 20.확인 위 tempEnd변수값으로 아래내용에 이용 시작페이지를 계산하게 됩니다.
 		this.startPage = (tempEnd - this.perPageNum) + 1;//UI페이지하단에 페이지번호가 출력되도록 하는 반복의 시작 변수.
+		//(20-10)+1 = 현재페이지의 UI하단의 시작페이지번호 11 (시작페이지)
 		//예, 1-10까지는 page를jsp에 클릭했을때, 시작페이지가 항상 1페이지, 하지만, 11부터20까지 시작페이지 위 계산식을 이용하면 항상 11페이지로 됩니다.
+		//위 startPage변수 jsp에서 반복문의 시작 값으로 사용.지금 토탈개수는 101개 이상
+		if(tempEnd*this.queryPerPageNum > this.totalCount) {
+			this.endPage = (int)Math.ceil((this.totalCount/(double)this.queryPerPageNum));
+			//위 계산식을 예를들면, (101/10) = ceil(10.1) = 11  (엔드페이지)
+		}
 		
 	}
 	public int getStartPage() {
