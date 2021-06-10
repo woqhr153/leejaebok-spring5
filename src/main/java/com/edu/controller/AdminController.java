47d4e23b4f5e709d728f56d1b32c94ac7b7fbc5e
boramcom@daum.net <boramcom@daum.net>
2021-06-10 16:34:37 +0900

    7교시 커밋

--- src/main/java/com/edu/controller/AdminController.java
+++ src/main/java/com/edu/controller/AdminController.java
@@ -1,10 +1,18 @@
 package com.edu.controller;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.edu.service.IF_MemberService;
+import com.edu.vo.MemberVO;
 import com.edu.vo.PageVO;
 
 /**
@@ -15,10 +23,26 @@ import com.edu.vo.PageVO;
  */
 @Controller
 public class AdminController {
+	//컨트롤러수정하면 자동로딩(auto컴파일)
+	//디버그용 로그객체 생성(아래)
+	private Logger logger = LoggerFactory.getLogger(AdminController.class);
 	//이 메서드는 회원목록을 출력하는 jsp와 매핑이 됩니다.
+	@Inject
+	private IF_MemberService memberService;
+	
 	@RequestMapping(value="/admin/member/member_list", method=RequestMethod.GET)
 	public String selectMember(PageVO pageVO) throws Exception {
-		//컨트롤러수정하면 자동로딩(auto컴파일)
+		//jsp의 검색버튼클릭시 search_type, search_keyword 내용이 PageVO클래스에 Set됩니다.
+		
+		//위에서 검색어를 받아서 역방향 검색한결과를 만들어서 jsp 보내줍니다.(아래)
+		if(pageVO.getPage() == null) {//jsp에서 전송값이 없을때만 초기값 입력
+			pageVO.setPage(1);
+		}
+		//pageVO의 calcPage메서드를 실행하려면, 필수 변수값입력(아래)
+		pageVO.setQueryPerPageNum(10);
+		logger.info("디버그" + pageVO.toString());//지금까지 jsp->컨트롤러 일방향 자료 이동.
+		List<MemberVO> listMember = memberService.selectMember(pageVO);
+		pageVO.setTotalCount(listMember.size());//검색되든 않되든 결과의 전체카운트값
 		return "admin/member/member_list";//jsp파일 상대경로
 	}
 	//URL요청 경로는 @RequestMapping 반드시 절대경로로 표시
