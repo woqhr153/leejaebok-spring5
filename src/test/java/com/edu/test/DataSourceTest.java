2d3e9e1ededd6ecd31a4c295fb09150a7069f3f6
boramcom@daum.net <boramcom@daum.net>
2021-06-10 11:50:45 +0900

    3교시 커밋

--- src/test/java/com/edu/test/DataSourceTest.java
+++ src/test/java/com/edu/test/DataSourceTest.java
@@ -13,6 +13,7 @@ import javax.sql.DataSource;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -42,6 +43,40 @@ public class DataSourceTest {
 	@Inject //MemberService서비스를 주입받아서 객체를 사용합니다.(아래)
 	private IF_MemberService memberService;
 	
+	@Test
+	public void updateMember() throws Exception {
+		//이 메서드는 회원 정보수정(1개 레코드). jsp에서 사용예정.
+		MemberVO memberVO = new MemberVO();
+		memberVO.setEmail("admin@test.com");
+		memberVO.setEnabled(true);
+		memberVO.setLevels("ROLE_ADMIN");
+		memberVO.setPoint(100);
+		memberVO.setUser_name("최고관리자");
+		memberVO.setUser_pw("1234");//1사이클 돌린후 암호화로직 적용.
+		//스프링5시큐리티 암호화 적용로직(아래)
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String userPwEncoder = passwordEncoder.encode(memberVO.getUser_pw());
+		memberVO.setUser_pw(userPwEncoder);//암호화된 해시데이터가 memberVO객체 임시저장됨.
+		memberVO.setUser_id("admin");//수정 조회조건에 사용.
+		//아래 수정 call호출을 회원수만큼 반복을 해야 합니다.(아래)
+		PageVO pageVO = new PageVO();
+		pageVO.setPage(1);//기본값으로 1페이지를 입력합니다.
+		pageVO.setPerPageNum(10);//UI하단사용 페이지 개수
+		pageVO.setQueryPerPageNum(1000);//쿼리사용 페이지당 개수
+		//MemberVO타입을 가진 리스트형 객체 List<MemberVO>
+		List<MemberVO> listMember = memberService.selectMember(pageVO);
+		//향상된 for반복문(memberOne:listMember) {구현내용}
+		for(MemberVO memberOne:listMember) { //listMember객체 비워질때까지 반복
+			//memberOne객체(1개의레코드)의 암호를 뽑아서 시큐리티로 암호화 시킨 후 onePwEncoder변수입력
+			String onePwEncoder = passwordEncoder.encode(memberOne.getUser_pw());
+			//혹시 여러번 실행시켜서 중복암호화 시킬수 있으므로 제외조건을 추가(아래)
+			if(onePwEncoder.length() < 10) {
+				memberOne.setUser_pw(onePwEncoder);
+				memberService.updateMember(memberOne);//1명(admin만) 수정 -> 모든회원을 업데이트
+			}
+		}
+		
+	}
 	@Test
 	public void readMember() throws Exception {
 		//이 메서드는 회원 상세보기(1개 레코드) jsp에 사용될 예정.
@@ -84,10 +119,12 @@ public class DataSourceTest {
 		
 		pageVO.setPage(1);//기본값으로 1페이지를 입력합니다.
 		pageVO.setPerPageNum(10);//UI하단사용 페이지 개수
-		pageVO.setQueryPerPageNum(10);//쿼리사용 페이지당 개수
-		pageVO.setTotalCount(memberService.countMember());//테스트하려고, 100명을 입력합니다.
+		pageVO.setQueryPerPageNum(1000);//쿼리사용 페이지당 개수
+		//pageVO.setTotalCount(memberService.countMember());//테스트하려고, 100명을 입력합니다.
+		/* 모든 사용자를 출력하지 않고, 일부 사용자만 출력할때 아래 2줄필요
 		pageVO.setSearch_type("user_id");//검색타입 all, user_id, user_name
 		pageVO.setSearch_keyword("user_del");//검색어
+		*/
 		//위 setTotalCount위치가 다른 설정보다 상단이면, 에러발생 왜냐하면, calcPage()가 실행되는데, 실행시 위 3가지변수값이 저정되 있어야지 계산메서드가 정상작동되기때문입니다.
 		//위토탈카운트변수값은 startPage, endPage계산에 필수입니다. 
 		//매퍼쿼리<-DAO클래스<-Service클래스<-JUnit(나중엔 컨트롤러에서작업) 이제 역순으로 작업진행
