7aa48c2752042d0266fd9ce63a4420e2ea15452a
boramcom@daum.net <boramcom@daum.net>
2021-06-15 14:59:29 +0900

    커밋

--- src/test/java/kr/or/test/SingletonApp.java
+++ src/test/java/kr/or/test/SingletonApp.java
@@ -4,8 +4,17 @@ import java.util.Calendar;
 
 /**
  * 싱글톤에 대한 설명: 1개의 클래스가 1개 객체(인스턴스)로만 생성해서 사용하겠다고 명시
+ * 장점.메모리 낭비를 방지하는 효과
+ * 예, DB접속객체를 예를 들면
+ * Connection conn1 = dataSource.getConnection();//싱글톤 객체 new키워드를 사용X
+ * connection객체가 생성되어 있으면,
+ * Connection conn2 = dataSource.getConnection();//싱글톤 객체
+ * Connection conn3 = dataSource.getConnection();//싱글톤 객체
+ * conn1==conn2==conn3 다 같은 동일객체로 같은 메모리를 사용합니다.즉, 1개의 객체만 생성됩니다.
+ * 결과적으론 1번 DB커넥션이 성공하면, 객체가 없어질때까지(null) 중복으로 생성되지 않습니다.
  * 즉, 객체를 2번 생성할 필요가 없는 로직일때 사용
- * 대표적인 싱클톤객체는 Calendar클래스, DB접속클래스등이 있습니다.
+ * 대표적인 싱클톤객체는 Calendar(달력)클래스, DB접속클래스등이 있습니다.
+ * Logger(로깅) 클래스(싱글톤)
  * @author User
  *
  */
