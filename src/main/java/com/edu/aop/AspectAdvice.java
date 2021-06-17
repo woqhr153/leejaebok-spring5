5c1d923e9c5d5d4d331c4d24b04523bd17f96aca
boramcom@daum.net <boramcom@daum.net>
2021-06-17 14:40:44 +0900

    5교시 커밋

--- src/main/java/com/edu/aop/AspectAdvice.java
+++ src/main/java/com/edu/aop/AspectAdvice.java
@@ -1,9 +1,32 @@
 package com.edu.aop;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.edu.service.IF_BoardTypeService;
+import com.edu.vo.BoardTypeVO;
+
 /**
- * 이 클래스는 AOP기능중 
- * @author User
+ * 이 클래스는 AOP기능 중 @Aspect 과 @ControllerAdvice 로 구현됩니다.
+ * @author 김일국
  *
  */
+@Aspect
+@ControllerAdvice
 public class AspectAdvice {
-
+	@Inject
+	private IF_BoardTypeService boardTypeService;
+	
+	//이 메서드는 컨트롤러의 메서드가 실행 전에 값을 생성해서 model객체에 담아서 jsp로 자료를 전송
+	//위 @컨트롤러어드바이스 를 이용해서 컨트롤러의 모든 메서드가 실행되기 호출만되면 아래 메서드가 자동실행(콜백함수) 
+	@ModelAttribute("listBoardTypeVO")
+	public List<BoardTypeVO> listBoardTypeVO() throws Exception {
+		
+		return boardTypeService.selectBoardType();
+	}
 }
--- src/main/java/com/edu/controller/AdminController.java
+++ src/main/java/com/edu/controller/AdminController.java
@@ -43,7 +43,8 @@ public class AdminController {
 	//왜 board_type하지않고, bbs_type하는 이유는 왼쪽메뉴 고정시키는 로직에서 경로 board와 겹치지 않도록
 	@RequestMapping(value="/admin/bbs_type/bbs_type_list", method=RequestMethod.GET)
 	public String selectBoardTypeList(Model model) throws Exception {//목록폼1
-		model.addAttribute("listBoardTypeVO", boardTypeService.selectBoardType());
+		//아래 모델은 AOP기능중 ControllerAdvice 인터페이스로 구현했기 때문에 아래는 실행안함.
+		//model.addAttribute("listBoardTypeVO", boardTypeService.selectBoardType());
 		return "admin/bbs_type/bbs_type_list";//상대경로일때는 views폴더가 root(최상위)
 	}
 	//bbs_type_list.jsp에서 게시판생성 버튼을 클릭했을때 이동하는 폼 경로 
