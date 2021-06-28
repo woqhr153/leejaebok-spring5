9f31a857c626e3021b614ccf6ed43ff474fe460d
boramcom@daum.net <boramcom@daum.net>
2021-06-28 14:41:02 +0900

    5교시 커밋

--- src/main/java/com/edu/controller/ReplyController.java
+++ src/main/java/com/edu/controller/ReplyController.java
@@ -10,12 +10,14 @@ import javax.inject.Inject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.edu.service.IF_ReplyService;
 import com.edu.vo.PageVO;
+import com.edu.vo.ReplyVO;
 
 /**
  * 이 클래스는 RestFull 서비스의 Endpoint(게이트웨이,URL매핑)를 만드는 클래스임.
@@ -28,8 +30,26 @@ public class ReplyController {
 	@Inject
 	private IF_ReplyService replyService;
 	
+	//댓글 등록 @RequestBody는 jsp에서 Ajax메서드로 보내온 값을 받을때 사용하는 애노테이션 입니다.
+	@RequestMapping(value="/reply/reply_insert", method=RequestMethod.POST)
+	public ResponseEntity<String> reply_write(@RequestBody ReplyVO replyVO) {
+		//ResponseEntity == ResponsBody
+		ResponseEntity<String> result = null;
+		//개발자가 스프링에 예외를 throws하지않고, 직접처리 try~catch하는 목적:
+		//Rest상태값(200,204,500등의 상황들)을 개발자가 보내줘야 하기 때문에...
+		try {
+			replyService.insertReply(replyVO);
+			result = new ResponseEntity<String>("success",HttpStatus.OK);//객체 생성시 매개변수로 상태값 + 입력성공시 success라는 문자열도 보냄
+			
+		} catch (Exception e) {
+			result = new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+		}
+		
+		return result;
+	}
 	@RequestMapping(value="/reply/reply_list/{bno}/{page}", method=RequestMethod.POST)//POST로 만든목적은 현재 도메인에서만 사용가능하도록 하기 위해서
 	public ResponseEntity<Map<String,Object>> reply_list(@PathVariable("bno")Integer bno,@PathVariable("page")Integer page) {
+		ResponseEntity<Map<String,Object>> result = null;
 		//ResponseEntity는 일반 Controller클래스에서 사용했던 ResponseBody와 같은 역할.
 		//URL주소가 아니고, Json데이터형으로 자료를 반환.
 		/*
@@ -56,7 +76,7 @@ public class ReplyController {
 		dummyMapList.add(1, dummyMap2);
 		dummyMapList.add(2, dummyMap3);
 		*/
-		ResponseEntity<Map<String,Object>> result = null;
+		
 		try {
 		PageVO pageVO = new PageVO();
 		pageVO.setPage(page);
@@ -72,6 +92,7 @@ public class ReplyController {
 			//아래 resultMap을 만든 목적은: 위 List객체를 ResponseEntity객체의 매개변수로 사용.
 			Map<String,Object> resultMap = new HashMap<String,Object>();
 			resultMap.put("replyList", replyService.selectReply(pageVO));
+			resultMap.put("pageVO", pageVO);
 			//객체를 2개 이상 보내게 되는 상황일때, Json데이터형태(key:value)로 만들어서 보냅니다. 
 			//--------------------------------------------------------
 			//result객체를 만든목적:RestApi클라이언트(jsp쪽)으로 resultMap객체를 보낼때 상태값을 위해서
